5f42efd095ab32afb13295db61508824682bbf33
Demon1056 <102851685+Demon1056@users.noreply.github.com>
2022-08-28 01:25:45 +0300

    Done  3 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+ require ('SimpleLightbox');
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryHtml= galleryItems.map(creatOneImage).join('')
+function creatOneImage ({preview, original, description}){
+    return `<a class="gallery__item" href = "${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       alt="${description}"
+     />
+ </a>
+ `; 
+}
+const galleryArea= document.querySelector ('.gallery')
+function createGallery() {
+  return galleryArea.insertAdjacentHTML('beforeend', galleryHtml)
+}
+createGallery()
 
-console.log(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData:'alt', captionDelay:250} );
